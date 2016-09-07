668f4a38e809ffad3f7cc9d1bec071680ad972ca
Joel Forman <league@WTS11.attlocal.net>
2016-09-07 16:59:23 -0700

    Worked on netflix program

--- Level1/src/NetflixRunner.java
+++ Level1/src/NetflixRunner.java
@@ -1,4 +1,18 @@
 
 public class NetflixRunner {
 
+	public static void main (String[] args)
+	{
+		Movie shortCircuit = new Movie("Short Circuit", 4);
+		Movie zootopia = new Movie("Zootopia", 4);
+		Movie avengers = new Movie("Avengers", 5);
+		Movie theRoom = new Movie("The Room", 1);
+		Movie imitationGame = new Movie("Imitation Game", 5);
+		System.out.println("A ticket to see Short Circuit? " + shortCircuit.getTicketPrice());
+		System.out.println("A ticket to see Zootopia? " + zootopia.getTicketPrice());
+		System.out.println("A ticket to see The Avengers? " + avengers.getTicketPrice());
+		System.out.println("A ticket to see The Room? " + theRoom.getTicketPrice());
+		System.out.println("A ticket to see Imitation Game? " + imitationGame.getTicketPrice());
+		NetflixQueue queue= new NetflixQueue();
+	}
 }
